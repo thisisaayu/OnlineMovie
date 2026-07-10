f0b04c7f3f73d3d54fc9526be64538a90cb6bebd
thisisaayu <4b2b7mn2gv@privaterelay.appleid.com>
2026-07-10 11:02:32 +0545

    made the light-dark toggle seamless

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@ $base = '/OnlineMovie';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?= $base ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?= $base ?>/Assets/Css/style.css">
 </head>
 <body>
 
